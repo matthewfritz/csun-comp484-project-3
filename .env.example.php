6ccf3de94f3286f385f3d02e29e698fbd346d2af
Matthew Fritz <matthew.fritz@csun.edu>
2017-03-08 15:40:28 -0800

    Added the env value that modifies how the user roles are stored in the session

--- .env.example.php
+++ .env.example.php
@@ -12,6 +12,7 @@ return [
 	// session configuration
 	'SESSION_USER_ID' => 'user_id', // this is where the user ID will be stored in the session
 	'SESSION_USER_OBJ' => 'user', // this is where the object representing the logged-in user will be stored in the session
+	'SESSION_USER_ROLES' => 'user_roles', // this is where the array representing the user's roles will be stored
 
 	// database configuration parameters
 	'DATABASE_HOST' => '', // host
